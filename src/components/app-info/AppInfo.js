63a44fdad22897856b3cf2f321e2fb834bf2c375
navruz22 <navruz.ochilov98@mail.ru>
2024-01-23 18:02:23 +0500

    done

--- src/components/app-info/AppInfo.js
+++ src/components/app-info/AppInfo.js
@@ -1,9 +1,9 @@
-import './appInfo.css'
+import './appInfo.scss'
 
 const AppInfo = ({ allMoviesCount, favouriteMoviesCount }) => {
   return (
     <div className='app-info'>
-      <p className="fs-3 text-uppercase">Barcha kinolar soni: { allMoviesCount }</p>
+      <p className="text-uppercase" style={{ fontSize: '1.1rem', fontWeight: 'bold' }}>Barcha kinolar soni: { allMoviesCount }</p>
       <p className="fs-4 text-uppercase">Sevimli kinolar soni: { favouriteMoviesCount }</p>
     </div>
   )
